6d0e9f175158d6acfb6546c9714901135dd30d05
Azzamubaidillah <azzam.ubaidillah05@gmail.com>
2021-01-03 18:10:21 +0700

    first fragment and activity

--- app/src/main/java/com/azzam/catatan/MainActivity.kt
+++ app/src/main/java/com/azzam/catatan/MainActivity.kt
@@ -2,10 +2,26 @@ package com.azzam.catatan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(HomeFragment.newInstance(), true)
+    }
+
+        fun replaceFragment(fragment: Fragment, itransition:Boolean ){
+            val fragmentTransition = supportFragmentManager.beginTransaction()
+
+            if (itransition) {
+                fragmentTransition.setCustomAnimations(
+                    android.R.anim.slide_out_right,
+                    android.R.anim.slide_in_left
+                )
+            }
+            fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 }
